053058ac776365a0a03db7dfbcc4d2868c7033bd
pulasthi <himaruwan2pulasthi@gmail.com>
2020-05-20 17:45:37 +0530

    commit

--- Sample-C-Plus/Task-8_5.cpp
+++ Sample-C-Plus/Task-8_5.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
 using namespace std;
 
+//initialize functions
+void reorder(int *a, int *b, int *c);
 int main(){
+    //initialize variables
+    int a,b,c;
+
+    //get the inputs from the user
+    cout<<"Insert three integers"<<endl;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+
+    //call the re-order function
+    reorder(*a, *b, *c);
     return 0;
+}
+
+void reorder(int *a, int *b, int *c){
+    //compare the numbers according to the order
+    if(*a>*b){
+        if(*b>*c){
+            cout<<"The Order is "<<*c<<","<<*b<<","<<*a<<endl;
+        }
+        else{
+            if(*c>*a){
+                cout<<"The Order is "<<*b<<","<<*a<<","<<*c<<endl;
+            }
+            else{
+                cout<<"The Order is "<<*b<<","<<*c<<","<<*a<<endl;
+            }
+        }
+    }
+    else{
+        if(*b>*c){
+           if(*a>*c){
+               cout<<"The Order is "<<*c<<","<<*a<<","<<*b<<endl;
+           }
+           else{
+               cout<<"The Order is "<<*a<<","<<*b<<","<<*c<<endl;
+           }
+        }
+        else{
+           cout<<"The Order is "<<*a<<","<<*c<<","<<*b<<endl; 
+        }
+    }
 }
