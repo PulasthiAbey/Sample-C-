19bc8e4d1ecc3a77461adfbe8d010e6b266ac264
pulasthi <himaruwan2pulasthi@gmail.com>
2020-05-21 22:38:07 +0530

    new

--- Sample-C-Plus/Task-9_2.cpp
+++ Sample-C-Plus/Task-9_2.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <iostream>
 using namespace std;
+
 //initializing the functions
-struct Emp get_data();
+struct Emp get_data(Emp []);
 void print_data(struct Emp[], int);
 double get_average(struct Emp*, int,string);
 double get_salary(struct Emp[], string);
@@ -22,11 +24,24 @@ struct Emp{
 int main(){
 
     //declaring the arrays
-    Emp employee[5];    
+    Emp employee[5];
+
+    //calling the functions
+    get_data(employee);    
 
     return 0;
 }
 
-struct Emp get_data(){
+struct Emp get_data(Emp w[5]){
+    int count = 0;
+    cout<<"Insert the Number of Employees : ";
+    cin>>count;
+
+    //insert details to the array
+    while (count!=0){
+        cout<<"Insert the name of the employee : ";
+        cin>>w[count].emp_name;
+    }
     
+
 }
