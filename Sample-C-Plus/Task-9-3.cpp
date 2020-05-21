488e6bb64fba1b9132d5c015472573dc532fe240
pulasthi <himaruwan2pulasthi@gmail.com>
2020-05-22 00:02:27 +0530

    New task started

--- Sample-C-Plus/Task-9-3.cpp
+++ Sample-C-Plus/Task-9-3.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+//initializin the enum for genre
+enum genre{
+    pop,
+    Jazz,
+    Classic
+};
+
+//initializing the struct
+struct album {
+    string album_name;
+    genre kind;
+    int trach_number;
+    string tracks[5];
+    string tracklocation;
+};
+
 int main(){
+
+
     return 0;
 }
