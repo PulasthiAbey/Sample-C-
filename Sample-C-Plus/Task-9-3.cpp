ac8c2fca4c6b51d7132f21ba0077708c158293df
pulasthi <himaruwan2pulasthi@gmail.com>
2020-05-22 00:58:08 +0530

    New file

--- Sample-C-Plus/Task-9-3.cpp
+++ Sample-C-Plus/Task-9-3.cpp
@@ -18,8 +18,51 @@ struct album {
     string tracklocation;
 };
 
-int main(){
 
+//initializing the functions
+void add_album(album);
+
+//main method
+int main(){
+    //struct instance
+    album w;
+
+    //initializing the program
+    cout<<"Enter the Option"<<endl;
+    cout<<"\t 1 to add an album"<<endl;
+    cout<<"\t 2 to print an album"<<endl;
+    cout<<"\t 3 to play an album"<<endl;
+    cout<<"\t 4 Exit"<<endl;
+
+    int x;
+    cin>>x;
+
+    //determinging the functions and activities
+    if(x==1){
+        add_album(w);
+    }
+    else {
+        if (x==2){
+
+        }
+        else {
+            if (x==3) {
+
+            }
+            else {
+                if (x==4){
+
+                }
+                else {
+                    cout<<"The inserted respose is not calid. Please run the program again"<<endl;  
+                }
+            }
+        }
+    }
 
     return 0;
+}
+
+void add_album(album w){
+
 }
