32ff05d3e12435734386387b45119b8e0e8aec8a
pulasthi <himaruwan2pulasthi@gmail.com>
2020-05-22 11:35:26 +0530

    Task Completed

--- Sample-C-Plus/Task-10-2.cpp
+++ Sample-C-Plus/Task-10-2.cpp
@@ -7,10 +7,68 @@ struct studentname {
     studentname *next;
 };
 
+//defining the functions
+bool isEmpty(studentname *head);
+void insertFirstElement(studentname *&head, studentname *&last, char letter);
+void insert(studentname *&head, studentname *&last, char letter);
+void showList(studentname *current);
+
 int main() {
+    studentname *head = NULL;
+    studentname *last = NULL;
 
+    //calling functions ffor the insertions
+    insertFirstElement(head, last, 'P');
+    insert(head, last, 'o');
+    insert(head, last, 'n');
+    insert(head, last, 't');
+    insert(head, last, 'i');
 
-
+    //diplaying the linked list
+    showList(head);
 
     return 0;
+}
+
+bool isEmpty(studentname *head){
+    if (head == NULL) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+void insertFirstElement(studentname *&head, studentname *&last, char letter) {
+    studentname *temp = new studentname;
+    temp->letter = letter;
+    temp->next = NULL;
+    head = temp;
+    last = temp;
+}
+
+void insert(studentname *&head, studentname *&last, char letter) {
+    if (isEmpty(head)){
+        insertFirstElement(head, last, letter);
+    }
+    else {
+        studentname *temp = new studentname;
+        temp->letter = letter;
+        temp->next = NULL;
+        last->next = temp;
+        last = temp; 
+    }
+}
+
+void showList(studentname *current){
+    if (isEmpty(current)){
+        cout<<"The List is Empty"<<endl;
+    }
+    else {
+        cout<<"The list Contains\n";
+        while (current != NULL) {
+            cout<<current->letter<<endl;
+            current = current->next;
+        }
+    }
 }
