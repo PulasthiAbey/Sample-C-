d80066a5350dce1961676e2c3931b89aa6683ec7
pulasthi <himaruwan2pulasthi@gmail.com>
2020-05-22 11:04:59 +0530

    linked list

--- Sample-C-Plus/Task-10-2.cpp
+++ Sample-C-Plus/Task-10-2.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 using namespace std;
 
+//initializing the struct
+struct studentname {
+    char letter;
+    studentname *next;
+};
+
 int main() {
 
-    
+
 
 
     return 0;
