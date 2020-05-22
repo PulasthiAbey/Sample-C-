55a78d8dfeb22308d3cbcced48ef6b055a4f4e8e
pulasthi <himaruwan2pulasthi@gmail.com>
2020-05-22 16:53:09 +0530

    New

--- Sample-C-Plus/Task-10-3.cpp
+++ Sample-C-Plus/Task-10-3.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+//initializing the self reference struct
+struct node {
+    int numbers;
+    struct node *next;
+}; 
+
+//initializing functions
 int main () {
 
 
